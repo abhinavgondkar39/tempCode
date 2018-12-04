b4dfc621c2c3c1e42ec9b5d5922bebde1090b47f
abhinavgondkar39 <abhinavgondkar39@gmail.com>
2018-12-04 12:28:42 +0530

    first commit

---
+++ src/main/java/com/ab9/GitHub/GitDemo/App.java
@@ -0,0 +1,13 @@
+package com.ab9.GitHub.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
